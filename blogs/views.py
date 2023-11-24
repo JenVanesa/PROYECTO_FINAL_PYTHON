098f04fcadb35c6c7d31c2c839c5db5fabe8a407
Jenny V <jenycita_13@hotmail.com>
2023-11-24 16:21:17 -0500

    Se genero vistas y templates

--- blogs/views.py
+++ blogs/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from blogs.models import Post, Categorias
+
 
 # Create your views here.
 def home_page(request):
-    return HttpResponse('Hola')
+   posts= Post.objects.all()
+   categorias= Categorias.objects.all()
+
+   context = {
+      'posts':posts,
+      'categorias':categorias      
+   }
+   return render (request, 'blogs/home_page.html', context=context)
+
