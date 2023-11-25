7e07724566e96faa2d67892a6cc44e78de0356d9
Jenny V <jenycita_13@hotmail.com>
2023-11-25 15:35:55 -0500

    Agregado cambios

--- blogs/views.py
+++ blogs/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
 
 from blogs.models import Post, Categorias
 
@@ -16,4 +17,8 @@ def home_page(request):
       'destacados': featured    
    }
    return render (request, 'blogs/home_page.html', context=context)
+
+class PostDetailView(generic.DetailView):
+   modelo = Post
+
 
