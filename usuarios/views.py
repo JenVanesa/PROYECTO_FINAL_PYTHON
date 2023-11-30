13db4b45233e94f6549140f81b44f2b53174ca67
Jenny V <jenycita_13@hotmail.com>
2023-11-29 21:40:06 -0500

    Se agregó ingreso de usuarios

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.edit import FormView
+
+from usuarios.forms import RegisterForm
 
 # Create your views here.
+
+class UserRegistration(FormView):
+    template_name= 'usuarios/registration.html'
+    form_class = RegisterForm
+    success_url=reverse_lazy('usuarios:success')
+
+    def form_valid(self, form):
+        form.save()
+        return super(UserRegistration, self).form_valid(form)
